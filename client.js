8e59e5cafa37fe26dbb0af02dc423cbf3300ed08
Stephane Bisson <stephane.c.bisson@gmail.com>
2012-10-31 08:55:18 +0800

    make client and server command line commands

--- client.js
+++ client.js
@@ -1,3 +1,7 @@
+#!/usr/bin/env node
+
+var serverAddress = process.argv[2] || 'http://localhost:3000';
+
 var request = require('request'),
     readline = require('readline');
 
@@ -25,7 +29,7 @@ var messageLoop = function(){
 var postMessage = function(name, message){
     request({
         method:'post', 
-        url: 'http://localhost:3000', 
+        url: serverAddress, 
         json:{name: name, message: message, timestamp: new Date()}
     });
 };
