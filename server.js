baf9f2049fddb30a0aadc0c76c806477ddeb3fb0
Stephane Bisson <stephane.c.bisson@gmail.com>
2012-11-01 12:25:05 +0800

    create a page that shows the messages in real-time

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 #!/usr/bin/env node
 
 var express = require('express'),
-    app = express();
+    app = express(), 
+    server = require('http').createServer(app), 
+    io = require('socket.io').listen(server);
     
 app.use(express.bodyParser());
+
+// it would be nice to use this approach to expose public files
+// for some reasons, it didnt work this morning
+//app.use(express.static(process.env.PWD + '/public'));
 
 var initialMessage = {name: 'stephane', message: 'welcome', timestamp: new Date()};
 var messages = [initialMessage];
@@ -12,11 +18,25 @@ app.get('/', function(req, res){
     res.status(200).send(messages);
 });
 
+app.get('/view', function(req, res){
+    res.sendfile('viewMessages.html');
+});
+
+app.get('/public/:file', function(req, res){
+    res.sendfile('./public/' + req.params.file);
+});
+
 app.post('/', function(req, res){
     console.log(req.body.name, ' : ', req.body.message);
     messages.push(req.body);
+    io.sockets.emit('newMessage', req.body);
     res.send(200);
 });
 
-app.listen(3000);
+io.sockets.on('connection', function (socket) {
+    socket.emit('newMessage', initialMessage);
+});
+
+server.listen(3000);
+
 console.log('Server running...');
